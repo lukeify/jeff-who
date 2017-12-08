b73e34c8e26ed592cefce1c51ed201430647f087
Luke <lukealization@gmail.com>
2017-12-09 11:19:14 +1300

    Initial commit
    Who?

---
+++ README.md
@@ -0,0 +1,29 @@
+# Jeff Who?
+
+Mostly pointless WebExtension compatible with Mozilla Firefox and Google Chrome that replaces "Jeff Bezos" with "Jeff Who?" on most webpages — along with a few other easter eggs.
+
+This extension should also support Microsoft Edge & Opera but it has not been tested with these browers. Issues to fix PRs welcome. 
+
+## Installation
+
+This extension is currently unsigned, and requires the user to install it manually. For instructions on how to do this, please read the documents below.
+
+It has been submitted for inclusion at addons.mozilla.org.
+
+### Mozilla Firefox
+
+https://blog.mozilla.org/addons/2015/12/23/loading-temporary-add-ons/
+
+### Google Chrome
+
+https://developer.chrome.com/extensions/getstarted#unpacked
+
+## Notes
+
+* This addon does not update the tab title if it contains "Bezos".
+* This addon does not update pages with dynamically loaded content.
+* This addon is WYSIWYG (What You See Is Who You Get) compliant. 
+
+## License
+
+The content of this repository is licensed under [The GNU General Public License (GPL) version 3](http://www.gnu.org/licenses/gpl-3.0.html). 
---
+++ jeff-who.js
@@ -0,0 +1,120 @@
+// jeff-who.js
+var namespace = typeof browser !== 'undefined' ? browser : chrome;
+
+(function() {
+    var globalData = {
+        regexes: {
+            who: /(Jeff(?:rey)?(?:[\s-])?(?:Preston[\s-])?)?(Bezos)('s?)?/gi,
+            bae: /(?:Elon\s)?Musk/gi,
+            suborbital: /New\sShepard/gi
+        },
+        replacements: {
+            who: function(match, p1, p2, p3, offset, string) {
+                var who = "";
+                if (p1 && p1.toLowerCase().indexOf("jeff") !== -1) {
+                    who += p1;
+                }
+                who += p2[0] === "b" ? "who?" : "Who?";
+                if (p3) {
+                    who += "'s";
+                }
+                return who;
+            },
+            bae: function(match, p1, offset, string) {
+                return "God Emperor " + match;
+            },
+            suborbital: function(match, offset, string) {
+                return "Suborbital New Shepherd";
+            }
+        }
+    };
+
+    var matchedNodes = [];
+    var hasFoundWhoMatch = false;
+
+    /**
+     * Recursively test nodes in the current document that are text nodes and have substrings containing what we are looking
+     * for.
+     *
+     * @param {Node} elem - The element to recurse over.
+     */
+    var recurse = function(elem) {
+        var length = elem.childNodes.length;
+        for (var i = 0; i < length; i++) {
+            var currentNode = elem.childNodes[i];
+            if (currentNode.nodeType == Node.TEXT_NODE) {
+
+                if (globalData.regexes.who.test(currentNode.textContent)) {
+                    hasFoundWhoMatch = true;
+                    matchedNodes.push({ node: currentNode, type: "who" });
+                }
+
+                if (globalData.regexes.bae.test(currentNode.textContent)) {
+                    matchedNodes.push({ node: currentNode, type: "bae" });
+                }
+
+                if (globalData.regexes.suborbital.test(currentNode.textContent)) {
+                    matchedNodes.push({ node: currentNode, type: "suborbital" });
+                }
+
+            } else {
+                recurse(currentNode);
+            }
+        }
+    };
+
+    /**
+     * WaPo.
+     */
+    var replaceWapoLogo = function() {
+        var logoElem = document.querySelector("#mastnav-container .wplogo");
+        if (logoElem) {
+            logoElem.src = namespace.extension.getURL("images/wawho.png");
+            return;
+        }
+
+        var storyLogoElem = document.querySelector("#logo-in-nav .main-logo");
+        if (storyLogoElem) {
+            storyLogoElem.src = namespace.extension.getURL("images/wawho-white.png");
+        }
+    };
+
+    /**
+     * Twitter.
+     */
+    var replaceTwitterBio = function() {
+        var bioElem = document.querySelector(".ProfileHeaderCard-bio.u-dir");
+        if (bioElem) {
+            bioElem.innerHTML = '<a href="https://www.youtube.com/watch?v=WG9EgxGsJuQ">https://www.youtube.com/watch?v=WG9EgxGsJuQ</a>';
+        }
+    };
+
+    /**
+     *
+     */
+    var replaceText = function() {
+        if (hasFoundWhoMatch) {
+            for (var i = 0; i < matchedNodes.length; i++) {
+                var currentMatch = matchedNodes[i];
+                currentMatch.node.textContent = currentMatch.node.textContent.replace(globalData.regexes[currentMatch.type], globalData.replacements[currentMatch.type]);
+            }
+        }
+    };
+
+    /**
+     * Begin script execution.
+     */
+    if (window.location.href.indexOf("washingtonpost") !== -1) {
+        hasFoundWhoMatch = true;
+        replaceWapoLogo();
+    }
+
+    if (window.location.href.indexOf("twitter.com/jeffbezos") !== -1) {
+        hasFoundWhoMatch = true;
+        replaceTwitterBio();
+    }
+
+    recurse(document.body);
+    console.log(matchedNodes.length);
+    replaceText();
+})();
