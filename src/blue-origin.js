4b1154aa5b8f937ec6e87b4652d971b8603eef84
Luke <lukealization@gmail.com>
2017-12-15 20:06:57 +1300

    update washington post logo, add blue origin site easter egg

---
+++ src/blue-origin.js
@@ -0,0 +1,8 @@
+var replace__blueorigin = function() {
+    var copycat = document.querySelector(".content > .white > h3"); // Talk about make it hard to query...
+    if (copycat) {
+        copycat.innerHTML = "WE'RE LIKE SPACEX, BUT SUBORBITAL";
+    }
+};
+
+replace__blueorigin();
