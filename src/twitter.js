aa5f713ba92baa0a927839f2bad7bf2e4bd48fd9
Luke <lukedavia@icloud.com>
2019-05-14 16:18:28 +1200

    [bugfixes] update wapo script to reflect changes to their site, change twitter link, make twitter changes work with query params.

--- src/twitter.js
+++ src/twitter.js
@@ -4,7 +4,7 @@
 var replace__twitter = function() {
     var dropIt = document.querySelector(".ProfileHeaderCard-bio.u-dir");
     if (dropIt) {
-        dropIt.innerHTML = '<a href="https://www.youtube.com/watch?v=WG9EgxGsJuQ">Drop it. https://www.youtube.com/watch?v=WG9EgxGsJuQ</a>';
+        dropIt.innerHTML = 'Drop it. <a href="https://www.youtube.com/watch?v=WG9EgxGsJuQ">https://www.youtube.com/watch?v=WG9EgxGsJuQ</a>';
     }
 };
 
--- src/washington-post.js
+++ src/washington-post.js
@@ -2,15 +2,38 @@
  * On washingtonpost.com, replace the banner image with an Amazon-stylised image of "The Washington Post".
  */
 var replace__washingtonpost = function() {
-    var logoElem = document.querySelector("#mastnav-container .wplogo");
-    if (logoElem) {
-        logoElem.src = namespace.extension.getURL("images/wawho.png");
+    var logoNode = document.querySelector("#mastnav-container .logo");
+    if (logoNode) {
+
+        // Remove the existing elements inside the logo node.
+        while (logoNode.firstChild) {
+            logoNode.removeChild(logoNode.firstChild);
+        }
+
+        // Create and append our image.
+        var imgNode = document.createElement("img");
+        imgNode.src = namespace.extension.getURL("images/wawho.png");
+        imgNode.alt = "Who?";
+        logoNode.appendChild(imgNode);
+
+        // Add some padding so that the image isn't crushed up against the navigation elements.
+        var mastnavNode = document.getElementById("mastnav-wrapper");
+        mastnavNode.style.paddingTop = "1em";
+
         return;
     }
 
-    var storyLogoElem = document.querySelector("#logo-in-nav .main-logo");
-    if (storyLogoElem) {
-        storyLogoElem.src = namespace.extension.getURL("images/wawho-white.png");
+    var storyLogoNode = document.querySelector("#logo-in-nav .wp-logo-link");
+    if (storyLogoNode) {
+        // Remove the first element, the SVG, from inside this logo node.
+        storyLogoNode.removeChild(storyLogoNode.firstElementChild);
+
+        // Create and append an image.
+        var imgNode = document.createElement("img");
+        imgNode.src = namespace.extension.getURL("images/wawho-white.png");
+        imgNode.alt = "The story is sponsored by Blue Origin.";
+        imgNode.style.width = "50%";
+        storyLogoNode.prepend(imgNode);
     }
 };
 
